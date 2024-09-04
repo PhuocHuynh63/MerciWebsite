12a7a0a810f83cce0a117856805df8009b8366c2
PhuocH <hmphuoc63@gmail.com>
2024-09-04 20:08:40 +0700

    done UI profile

--- src/pages/profileUser/ProfileUser.jsx
+++ src/pages/profileUser/ProfileUser.jsx
@@ -1,3 +1,5 @@
+import './ProfileUser.scss';
+
 const ProfileUser = () => {
     return (
         <div className="profile-user">
@@ -5,6 +7,34 @@ const ProfileUser = () => {
                 <h4>HỒ SƠ CỦA TÔI</h4>
                 <p>Quản lý thông tin hồ sơ để bảo mật tài khoản</p>
             </div>
+
+            <form>
+                <div className="form-group">
+                    <div className="form-input name">
+                        <label htmlFor="name">Họ và tên: <span> (*)</span></label>
+
+                        <input type="text" placeholder="Họ và tên" />
+                    </div>
+                    <div className="form-input birthday">
+                        <label htmlFor="birthday">Ngày sinh: <span> (*)</span></label>
+                        <input type="date" />
+                    </div>
+                    <div className="form-input phone">
+                        <label htmlFor="phone">Số điện thoại: <span> (*)</span></label>
+
+                        <input type="tel" placeholder="Số điện thoại" />
+                    </div>
+                    <div className="form-input email">
+                        <label htmlFor="email">Email: <span> (*)</span></label>
+
+                        <input type="email" placeholder="Email" />
+                    </div>
+
+                    <div className="btn col-9">
+                        <button className='btn-save'>Cập nhật</button>
+                    </div>
+                </div>
+            </form>
         </div>
     );
 }
