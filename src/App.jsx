30073d994c305b2135d04feec989252aeae46dd0
PhuocHuynh63 <phuochmse171830@fpt.edu.vn>
2024-10-10 18:55:28 +0700

    cart

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import ProfileUser from './pages/profileUser/ProfileUser';
 import ChangePassword from './pages/changePasswordPage/ChangePassword';
 import PurchaseOrder from './pages/purchaseOrder/PurchaseOrder';
 import Dashboard from './pages/adminPage/dashboard/Dashboard';
+import ComboDetail from './pages/comboDetail/ComboDetail';
 
 
 function App() {
@@ -24,7 +25,8 @@ function App() {
           <Route path='*' element={<Layout Component={NotFound} />} />
           <Route path='/' element={<Layout Component={HomePage} />} />
           <Route path='/about-us' element={<Layout Component={AboutUs} />} />
-          <Route path='/product/:id' element={<Layout Component={ProductDetail} />} />
+          <Route path='/product/:slug' element={<Layout Component={ProductDetail} />} />
+          <Route path='/combo/:slug' element={<Layout Component={ComboDetail} />} />
           <Route path='/cart' element={<Layout Component={Cart} />} />
           <Route path='/list-product/:id' element={<Layout Component={ListProduct} />} />
 
--- src/pages/shoppingCart/Cart.jsx
+++ src/pages/shoppingCart/Cart.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './Cart.scss';
 import { Link } from 'react-router-dom';
 import Checkout from '../popUp/checkOut/Checkout';
@@ -52,75 +52,120 @@ const Cart = () => {
     }
     //-------------End handle input quantity-------------//
 
+
+    /**
+      * Get cart items from local storage
+      */
+    const [cartItems, setCartItems] = useState([]);
+
+
+    useEffect(() => {
+        const storedCartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+        setCartItems(storedCartItems);
+    }, []);
+
+
+    // Remove item from cart
+    const handleRemoveItem = (productId) => {
+        const updatedCartItems = cartItems.filter(item => item.productId !== productId);
+        setCartItems(updatedCartItems);
+        localStorage.setItem('cartItems', JSON.stringify(updatedCartItems));
+    }
+    //-------------------End remove item from cart-------------------//
+
+
+    /**
+     * Total price of all items in cart
+     * @returns Total price of all items in cart
+     */
+    const getTotalPrice = () => {
+        return cartItems.reduce((total, item) => total + item.totalPrice, 0).toLocaleString('vi-VN');
+    };
+    //-------------------End total price of all items in cart-------------------//
+
     return (
         <div className="cart">
             <Checkout show={show} handleClose={handleClose} />
-            <div className="container">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Sản phẩm</th>
-                            <th>Mô tả</th>
-                            <th>Đơn giá</th>
-                            <th>Số lượng</th>
-                            <th>Tổng</th>
-                            <th>Thao tác</th>
-                        </tr>
-                    </thead>
 
-                    <tbody>
-                        <tr>
-                            <td>
-                                <img src="https://res.cloudinary.com/dwyzqcunj/image/upload/v1724224158/Remove-bg.ai_1720876902030_1_mbi9bc.svg" alt="" />
-                                <div className="background">
-                                    <img src="https://res.cloudinary.com/dwyzqcunj/image/upload/v1724834860/%C3%B4_2_iw8uvm.svg" alt="product" />
-                                </div>
-                            </td>
-                            <td>Tinh dầu sả chanh 5ml</td>
-                            <td>50.000đ</td>
-                            <td className='quantity'>
-                                <button value="-" onClick={() => toggleQuantity("-")}>-</button>
-                                <input
-                                    type="text"
-                                    onChange={handleQuantityChange}
-                                    value={quantity}
-                                    onKeyPress={handleKeyPress}
-                                    onMouseLeave={handleMouseLeave}
-                                />
-                                <button value="+" onClick={() => toggleQuantity("+")}>+</button>
-                            </td>
-                            <td>50.000đ</td>
-                            <td>
-                                <span>Xoá</span>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-
-                <div className="total">
-                    <span>Tổng: 50.000đ</span>
-                </div>
-
-                <div className="action">
+            {cartItems.length === 0 ?
+                <div className="empty-cart">
+                    {/* <img src="https://res.cloudinary.com/dwyzqcunj/image/upload/v1724224158/Remove-bg.ai_1720876902030_1_mbi9bc.svg" alt="" /> */}
+                    <p>Giỏ hàng của bạn đang trống</p>
                     <Link to='/'>
                         <button>
-                            <span>Tiếp tục mua hàng</span>
+                            <span>Quay lại trang chủ</span>
                         </button>
                     </Link>
-                    <button onClick={handleShow}>
-                        <span>Thanh toán</span>
-                    </button>
                 </div>
-            </div>
-            <div className="background">
+                : (
+                    <div className="container">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Sản phẩm</th>
+                                    <th>Mô tả</th>
+                                    <th>Đơn giá</th>
+                                    <th>Số lượng</th>
+                                    <th>Tổng</th>
+                                    <th>Thao tác</th>
+                                </tr>
+                            </thead>
+                            {cartItems.map((item, index) => (
+
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <img src={item.image} alt="" />
+                                            <div className="background">
+                                                <img src="https://res.cloudinary.com/dwyzqcunj/image/upload/v1724834860/%C3%B4_2_iw8uvm.svg" alt="product" />
+                                            </div>
+                                        </td>
+                                        <td>{item.productName}</td>
+                                        <td>{item.salePrice.toLocaleString()}đ</td>
+                                        <td className='quantity'>
+                                            <button value="-" onClick={() => toggleQuantity("-")}>-</button>
+                                            <input
+                                                type="text"
+                                                onChange={handleQuantityChange}
+                                                value={item.quantity}
+                                                onKeyPress={handleKeyPress}
+                                                onMouseLeave={handleMouseLeave}
+                                            />
+                                            <button value="+" onClick={() => toggleQuantity("+")}>+</button>
+                                        </td>
+                                        <td> {(item.quantity * item.salePrice).toLocaleString()}đ</td>
+                                        <td>
+                                            <span>Xoá</span>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            ))}
+                        </table>
+                        <div className="total">
+                            <span>{getTotalPrice().toLocaleString()} đ</span>
+                        </div>
+
+                        <div className="action">
+                            <Link to='/'>
+                                <button>
+                                    <span>Tiếp tục mua hàng</span>
+                                </button>
+                            </Link>
+                            <button onClick={handleShow}>
+                                <span>Thanh toán</span>
+                            </button>
+                        </div>
+                    </div>
+                )
+            }
+            < div className="background">
                 <div className="background-cloud">
                     <img src="https://res.cloudinary.com/dwyzqcunj/image/upload/v1724761940/m%C3%A2y_1_tcmrxx.svg" alt="" />
                 </div>
                 <div className="background-cloud2">
                     <img src="https://res.cloudinary.com/dwyzqcunj/image/upload/v1724761940/m%C3%A2y_1_tcmrxx.svg" alt="" />
                 </div>
-            </div>
-
+            </div >
         </div>
     );
 }
